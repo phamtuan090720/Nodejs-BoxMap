8bcd06d57de98caf494654f67d685c4ce33d5552
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-11-17 01:07:02 +0700

    Add Limit

--- src/utils/getCoordinates.js
+++ src/utils/getCoordinates.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const getCoordinates = (kw) => {
     const access_token = 'eyJ1IjoicGhhbXR1YW4wOTA3IiwiYSI6ImNrdzFpdDRtODAzenYyd3BkbDg1N3l2Y3QifQ.Ip2TU1DWhGY0tRxiaR4jww';
-    const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${kw}.json?access_token=pk.${access_token}`;
+    const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${kw}.json?limit=10&access_token=pk.${access_token}`;
     // giúp tìm kiếm được các kw tiếng việt trong axios
     const url = encodeURI(uri)
     return axios.get(url).then(rs => rs.data.features).catch(err => err)
